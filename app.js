c6e38e707542ff58f32de688197aa29425312c98
Shieda <drivnder123@gmail.com>
2022-09-20 21:38:39 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// https://www.omnicalculator.com/health/blood-sugar
+
+const v =  document.getElementById('v');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+// radio buttons
+const convertedRadio = document.getElementById('convertedRadio');
+const glucoselevelRadio = document.getElementById('glucoselevelRadio');
+
+let converted;
+let glucoselevel = v; 
+
+// labels of the inpust
+const variable = document.getElementById('variable');
+
+convertedRadio.addEventListener('click', function() {
+  variable.textContent = 'Glucose level';
+  glucoselevel = v;
+  result.textContent = '';
+});
+
+glucoselevelRadio.addEventListener('click', function() {
+  variable.textContent = 'Converted';
+  converted = v;
+  result.textContent = '';
+});
+
+btn.addEventListener('click', function() {
+  
+  if(convertedRadio.checked)
+    result.textContent = `Converted = ${computeconverted().toFixed(5)}`;
+
+  else if(glucoselevelRadio.checked)
+    result.textContent = `Glucose level = ${computeglucoselevel().toFixed(5)}`;
+})
+
+// calculation
+
+function computeconverted() {
+  return Number(glucoselevel.value) * 18;
+}
+
+function computeglucoselevel() {
+  return Number(converted.value) / 18;
+}
